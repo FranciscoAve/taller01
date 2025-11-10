1da4c7f39bbc0eef3cd2c4f5f2eca63de78f7d7d
Francisco Avendaño <favendan@espol.edu.ec>
2025-11-10 19:49:25 +0000

    aaa

--- taller01_short/scripts/main.js
+++ taller01_short/scripts/main.js
@@ -1,19 +1,7 @@
+"use strict";
+
 // Plantilla para cada canción:
-let card_song = `<article class="song-card">
-    <div class="cover">
-        <img src="https://i.ytimg.com/vi/V9PVRfjEBTI/maxresdefault.jpg"
-            alt="Portada: Billie Eilish - BIRDS OF A FEATHER (Official Music Video)">
-        <span class="badge">3:51:00</span>
-    </div>
-    <div class="content">
-        <h2 class="title">Billie Eilish - BIRDS OF A FEATHER (Official Music Video)</h2>
-        <div class="meta">558,329,099 vistas</div>
-        <div class="footer">
-            <span class="channel">Canal: <a href="https://www.youtube.com/channel/UCDGmojLIoWpXok597xYo8cg"
-                    target="_blank" rel="noopener noreferrer">Billie Eilish</a></span>
-        </div>
-    </div>
-</article>`
+
 
 // Formateador de números
 let formatter = new Intl.NumberFormat('en-US');
@@ -24,3 +12,56 @@ let formatter = new Intl.NumberFormat('en-US');
 
 // let views_withformat= formatter.format(views_witoutformat); 
 // Valor de views_withformat: "2,536,628"
+const contenedorCanciones = document.getElementsByClassName("songs-grid");
+
+const cargaAsincronicaDatos =async ()=>{
+    try{
+        const response = await fetch("https://raw.githubusercontent.com/DATA-DAWM/Datos/refs/heads/main/Youtube/only_songs.json");
+
+        if(!response.ok){
+            console.log(response.status);
+        }else{
+            const data = response.json();
+            let canciones = data.slice(0,12);
+
+            canciones.forEach(cancion => {
+                    let card_song = `<div class="cover">
+                                        <img src="[CANCION.THUMBNAIL]"
+                                                alt="Portada: [CANCION.TITTLE]">
+                                        <span class="badge">[CANCION.DURATION_STRING]</span>
+                                    </div>
+                                    <div class="content">
+                                        <h2 class="title">[CANCION.TITTLE]</h2>
+                                        <div class="meta">[CANCION.VIEW_COUNT] vistas</div>
+                                        <div class="footer">
+                                        <span class="channel">Canal: <a href="[CANCION.CHANNEL_URL]"
+                                        target="_blank" rel="noopener noreferrer">[CANCION.CHANNEL]</a></span>
+                                        </div>
+                                    </div>`
+
+                    card_song = card_song.replaceAll('[CANCION.THUMBNAIL]',cancion.thumbnail);
+                    card_song = card_song.replaceAll('[CANCION.TITTLE]',cancion.title);
+                    card_song = card_song.replaceAll('[CANCION.DURATION_STRING]',cancion.duration_string);
+
+                    let cancion_view_count = formatter.format(cancion.view_count);
+
+                    card_song = card_song.replaceAll('[CANCION.VIEW_COUNT]',cancion_view_count);
+                    card_song = card_song.replaceAll('[CANCION.CHANNEL_URL]',cancion.channel_url);
+                    card_song = card_song.replaceAll('[CANCION.CHANNEL]',cancion.channel);
+                    
+                    contenedorCanciones.innerHTML+= card_song;
+            });
+
+        }
+    }catch(error){
+        console.log(error.message);
+    }
+    
+
+}
+
+
+
+( ()=>{
+    cargaAsincronicaDatos(); 
+} ) ();
